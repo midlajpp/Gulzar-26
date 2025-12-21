3a0beab969a24b3933a784477ac1e5aa0d19768f
midlajpp <midlajppparappanangadi@gmail.com>
2025-12-21 13:19:34 +0530

    Just Modified

--- backend/models/Participant.js
+++ backend/models/Participant.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const participantSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  categories: [{ type: String, required: true }],
+  category: [{ type: String, required: true }],
   team: {
     name: { type: String, required: true },
   },
